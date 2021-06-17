6ef1fe3768ce9a0c3dbe2778e2cf1a63e0c17aa8
Jtobyy <jtobi8161@gmail.com>
2021-06-17 11:14:04 -0700

    Use betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-
+/**
+ * main - test puts
+ * Return: 0
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

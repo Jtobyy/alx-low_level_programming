121a8e70903301b5b7d2419207a55456e361ff9f
Jtobyy <jtobi8161@gmail.com>
2021-06-21 22:26:44 +0100

    Correct array lenght"

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -7,6 +8,6 @@
 int main(void)
 {
 char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fwrite(str, 1, sizeof(str), stderr);
+fwrite(str, 59, 1, stderr);
 return (1);
 }

521c8c3d93dedd58a138f838630851fb99dcdcc0
Jtobyy <jtobi8161@gmail.com>
2021-06-24 17:38:46 +0000

    Fix bug with leap year testing ontask 3

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
 
 #endif /* HOLBERTON_H */

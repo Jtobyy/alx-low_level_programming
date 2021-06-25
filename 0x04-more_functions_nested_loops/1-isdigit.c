403491fb85c668cf52e2ab61f607deef168d7add
Jtobyy <jtobi8161@gmail.com>
2021-06-25 07:19:42 +0000

    Fix merge conflict

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks to see if a givin value is a digit
+ * @c: character to be tested
+ * Return: 1 if true else 0
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}

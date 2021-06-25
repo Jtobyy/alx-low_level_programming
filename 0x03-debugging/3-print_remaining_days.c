cb3f6ad366c5b2bd5bd3b61c6014211301697592
Jtobyy <jtobi8161@gmail.com>
2021-06-25 07:07:09 +0000

    Implement isupper()

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,9 +14,16 @@ void print_remaining_days(int month, int day, int year)
 {
 if ((!(year % 4 == 0 && year % 100 == 0) && year % 4 == 0) || year % 400 == 0)
 {
-if ((month >= 2 && day >= 60) && (month >= 2 && day != 60))
+if (month >= 2 && day >= 60)
+{
+if (month == 2 && day == 60)
+{
+continue;
+}
+else
 {
 day++;
+}
 }
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);

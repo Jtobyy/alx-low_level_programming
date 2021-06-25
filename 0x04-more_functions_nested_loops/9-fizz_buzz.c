be673bdb66e3cf05e41eab02f367a23b5fcb0a10
Jtobyy <jtobi8161@gmail.com>
2021-06-25 15:42:35 +0000

    Work on 9-fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,116 +1,188 @@
 #include "holberton.h"
 
 /**
- * main -  a program that prints the numbers from 1 to 100, 
- *followed by a new line. But for multiples of three print Fizz 
- *instead of the number and for the multiples of five print Buzz. 
- *For numbers which are multiples of both three and five print FizzBuzz.
+ * main -  a program that prints the numbers from 1 to 100,
+ *followed by a new line. But for multiples of three print Fizz
+ *instead of the number and for the multiples of five print Buzz.
+ *For numb.
  * Return: 0
  */
 int main(void)
 {
-  int i;
-  for (i = 1; i <= 100; i++)
-    {
-      if (i % 3 == 0 && i % 5 != 0)
-	{
-	  putchar('F');
-	  putchar('i');
-	  putchar('z');
-	  putchar('z');
-	  putchar(' ');
-	  continue;
-	}
-      if (i % 5 == 0 && i % 3 != 0)
-	{
-	  putchar('B');
-	  putchar('u');
-	  putchar('z');
-	  putchar('z');
-	  putchar(' ');
-	  continue;
-	}
-      if (i % 3 == 0 && i % 5 == 0)
-	{
-	  putchar('F');
-	  putchar('i');
-	  putchar('z');
-	  putchar('z');
-	  putchar('B');
-	  putchar('u');
-	  putchar('z');
-	  putchar('z');
-	  putchar(' ');
-	  continue;
-	}
-      if (i >= 10 && i < 20)
-	{
-	  int t = i % 10;
-	  putchar(1 + '0');
-	  putchar(t + '0'); 
-	}
-      else if (i >= 20 && i < 30)
-	{
-	  int t = i % 10;
-	  putchar(2 + '0');
-	  putchar(t + '0'); 
-	}
-      else if (i >= 30 && i < 40)
-	{
-	  int t = i % 10;
-	  putchar(3 + '0');
-	  putchar(t + '0'); 
-	}
-      else if (i >= 40 && i < 50)
-	{
-	  int t = i % 10;
-	  putchar(4 + '0'); 
-	  putchar(t + '0'); 
-	}
-      else if (i >= 50 && i < 60)
-	{
-	  int t = i % 10;
-	  putchar(5 + '0');
-	  putchar(t + '0'); 
-	}
-      else if (i >= 60 && i < 70)
-	{
-	  int t = i % 10;
-	  putchar(6 + '0');
-	  putchar(t + '0'); 
-	}
-      else if (i >= 70 && i < 80)
-	{
-	  int t = i % 10;
-	  putchar(7 + '0');
-	  putchar(t + '0'); 
-	}
-      else if (i >= 80 && i < 90)
-	{
-	  int t = i % 10;
-	  putchar(8 + '0');
-	  putchar(t + '0');
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+fizzbuzz('f');
+continue;
+}
+if (i % 5 == 0 && i % 3 != 0)
+{
+fizzbuzz('b');
+continue;
+}
+if (i % 3 == 0 && i % 5 == 0)
+{
+fizzbuzz('a');
+continue;
+}
 
-	}
-      else if (i >= 90 && i < 100)
-	{
-	  int t = i % 10;
-	  putchar(9 + '0');
-	  putchar(t + '0');
-
-	}
-      else if (i == 100)
-	{
-	  putchar(1);
-	  putchar(0);
-	  putchar(0);
-	}
-      else
-	{
-	  putchar(i + '0');
-	}
-      putchar(' ');
-    }
-  putchar('\n');
+ 
+if (i >= 10 && i < 20)
+{
+  int t = i % 10;
+  g_putchar(t, 'a');
+ }
+else if (i >= 20 && i < 30)
+{
+   int t = i % 10;
+   g_putchar(t, 'b'); 
+}
+else if (i >= 30 && i < 40)
+{
+   int t = i % 10;
+   g_putchar(t, 'c');
+ }
+else if (i >= 40 && i < 50)
+{
+   int t = i % 10;
+   g_putchar(t, 'd');
+ }
+else if (i >= 50 && i < 60)
+{
+   int t = i % 10;
+   g_putchar(t, 'e');
+ }
+else if (i >= 60 && i < 70)
+{
+   int t = i % 10;
+   g_putchar(t, 'f');
+ }
+else if (i >= 70 && i < 80)
+{
+  g_putchar(t, 'g');
+ }
+else if (i >= 80 && i < 90)
+{
+  g_putchar(t, 'h');
+ }
+else if (i >= 90 && i < 100)
+{
+  g_putchar(t, 'i');
+}
+else if (i == 100)
+{
+  g_putchar(t, 'j');
+}
+else
+{
+putchar(i + '0');
+}
+putchar(' ');
+}
+putchar('\n');
 return (0);
 }
+
+/**
+ *fizzbuzz - checks if a number is a multiple of 3, 5 or
+ *both
+ *@x: indicator like a flag that tells checker what iteration it is
+ */
+void fizzbuzz(char x)
+{
+switch (x)
+{
+case 'f':
+putchar('F');
+putchar('i');
+putchar('z');
+putchar('z');
+putchar(' ');
+break;
+case 'b':
+putchar('B');
+putchar('u');
+putchar('z');
+putchar('z');
+putchar(' ');
+break;
+case 'a':
+putchar('F');
+putchar('i');
+putchar('z');
+putchar('z');
+putchar('B');
+putchar('u');
+putchar('z');
+putchar('z');
+putchar(' ');
+break;
+}
+return;
+}
+
+/**
+ *g_putchar: check for the the first digit of an iteration
+ *in order to avoid putchar anomaly
+ *@x: indicator as well
+ *@y: last intege
+ */
+void g_putchar(int y, char x)
+{
+  switch(x)
+    {
+    case 'a':
+
+      putchar(1 + '0');
+putchar(t + '0');
+ break;
+    case 'b':
+ int t = i % 10;
+putchar(2 + '0');
+putchar(t + '0');
+ break;
+    case 'c':
+int t = i % 10;
+putchar(3 + '0');
+putchar(t + '0');
+ break;
+    case 'd':
+int t = i % 10;
+putchar(4 + '0');
+putchar(t + '0');
+ break;
+    case 'e':
+int t = i % 10;
+putchar(5 + '0');
+putchar(t + '0');
+ break;
+    case 'f':
+int t = i % 10;
+putchar(6 + '0');
+putchar(t + '0');
+ break;
+    case 'g':
+int t = i % 10;
+putchar(7 + '0');
+putchar(t + '0');
+ break;
+    case 'h':
+int t = i % 10;
+putchar(8 + '0');
+putchar(t + '0');
+break
+    case 'i':
+int t = i % 10;
+putchar(9 + '0');
+putchar(t + '0');
+ break;
+    case 'j':
+putchar(1);
+putchar(0);
+putchar(0);
+ break;
+    }
+
+  
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -13,5 +13,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void fizzbuzz(char x);
+void g_putchar(int y, char x);
 
 #endif /* HOLBERTON_H */

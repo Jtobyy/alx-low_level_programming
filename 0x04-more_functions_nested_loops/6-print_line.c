df5c3673812f0a3cbe6e9c64d027768ad398dbd9
Jtobyy <jtobi8161@gmail.com>
2021-06-25 13:19:47 +0000

    Print more_numbers - 5

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,12 +12,12 @@ for (x = 0; x < 10; x++)
 {
 for (i = 0; i <= 14; i++)
 {
-if (i <= 9)
+if (i < 9)
 {
 putchar(i + '0');
 continue;
 }
-else if (i > 9)
+else if (i > 10)
 {
 int t = i % 10;
 putchar(1 + '0');

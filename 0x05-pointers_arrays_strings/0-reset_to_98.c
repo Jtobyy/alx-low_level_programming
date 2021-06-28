53df46d5cd24d54ee62fbb4b038b774284205527
Jtobyy <jtobi8161@gmail.com>
2021-06-28 16:27:19 +0000

    Pointers initaial commit 0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+#include <stdio.h>
+
+void reset_to_98(int *n)
+{
+*n = 98;
+return;
+}
---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+int main(void)
+{
+  int n;
+
+  n = 402;
+  printf("n=%d\n", n);
+  reset_to_98(&n);
+  printf("n=%d\n", n);
+  return (0);
+}

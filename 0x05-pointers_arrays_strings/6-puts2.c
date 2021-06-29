016ff29239ce7b680c55a831930c328088049d42
Jtobyy <jtobi8161@gmail.com>
2021-06-29 18:48:59 +0000

    6, Fixing bug of gabbage data getting printed out

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,11 @@ void puts2(char *str)
 int i;
 char tmp;
 i = 0;
-while (*(str + i) != 0)
-{
+do {
 tmp = *(str + i);
 putchar(tmp);
 i += 2;
-}
-putchar('\0');
+} while (*(str + i) != 0 && *(str + (i - 1)) != 0);
 putchar('\n');
 return;
 }

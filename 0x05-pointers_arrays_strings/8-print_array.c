0d5e875fa8674875c6e305fa416b64cc49537b6b
Jtobyy <jtobi8161@gmail.com>
2021-06-29 17:38:22 +0000

    Add 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_array - prints n elements of an array of ints
+ *@a: pointer to an array of ints
+ *@n: no of elments to print
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+int tmp;
+i = 0;
+while (i < n)
+{
+tmp = *(a + i);
+printf("%d", tmp); 
+if (i != (n - 1))
+printf(", ");
+i++;
+}
+putchar('\n');
+return;
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -45,9 +45,18 @@ int main(void)
     puts2(str);
     return (0); */
 
-  char *str;
+  /**  char *str;
 
   str = "0123456789";
   puts_half(str);
+  return (0); */
+  int array[5];
+
+  array[0] = 98;
+  array[1] = 402;
+  array[2] = -198;
+  array[3] = 298;
+  array[4] = -1024;
+  print_array(array, 5);
   return (0);
 }

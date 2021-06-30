1cd82eb6e1a98d79d0bb06bc150363faa1f6692b
Jtobyy <jtobi8161@gmail.com>
2021-06-30 10:20:45 +0000

    Initial commit for 0x06: _strcat, concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ *_strcat - concatenates two strings, appends source to destination
+*@dest: destination
+*@src: source
+*Return: pointer to destination
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+int k;
+i = 0;
+j = 0;
+while (*(src + i) != 0)
+{
+i++;
+}
+while (*(dest + j) != 0)
+{
+j++;
+}
+for (k = 0; k < i; k++)
+{
+*(dest + j) = *(src + k);
+j++;
+}
+*(dest + j) = '\0';
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src);
+#endif
+

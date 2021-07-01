6acb523ed2c9d06d81bc89f592a525fdf020da77
Jtobyy <jtobi8161@gmail.com>
2021-07-01 17:49:52 +0000

    Convert tabs and newlines when capitalizes to ordiry spaces in function 6-cap_string

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -61,29 +61,30 @@ int main (void)
   printf("%d\n", _strcmp(s2, s1));
   printf("%d\n", _strcmp(s1, s1));
   return (0); */
-
-  int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+  /**  int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
   print_array(a, sizeof(a) / sizeof(int));
   reverse_array(a, sizeof(a) / sizeof(int));
   print_array(a, sizeof(a) / sizeof(int));
-  return (0); 
-  /**  char str[] = "Holberton School!\n";
+  return (0); */
+  /**  
+char str[] = "Holberton School!\n";
   char *ptr;
 
   ptr = string_toupper(str);
   printf("%s", ptr);
   printf("%s", str);
   return (0); */
-  /**  char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+  
+  char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
   char *ptr;
 
-  ptr = leet(str);
+  ptr = cap_string(str);
   printf("%s", ptr);
   printf("%s", str);
-  return (0); */
+  return (0);
 }
-
+/**
 void print_array(int *a, int n)
 {
   int i;
@@ -102,3 +103,4 @@ void print_array(int *a, int n)
   return;
 }
   
+*/

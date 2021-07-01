195d4dcfe0fa5c9a154c5a8a6d1b9cc8161c7fcc
Jtobyy <jtobi8161@gmail.com>
2021-07-01 17:15:25 +0000

    Let _strcmp get return int from the difference btw the characters

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,6 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 int i;
 int j;
+int k;
 i = 0;
 while (*(s1 + i) != 0)
 {
@@ -18,12 +19,10 @@ i++;
 }
 for (j = 0; j < i; j++)
 {
-if (*(s1 + j) < *(s2 + j))
-return (-15);
-else if (*(s1 + j) == *(s2 + j))
+k = *(s1 + j) - *(s2 + j);
+if (k == 0)
 continue;
-else if (*(s1 + j) > *(s2 + j))
-return (15);
+break;
 }
-return (0);
+return (k);
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -6,7 +6,7 @@
  */
 int main (void)
 {
-  char s1[98] = "Hello ";
+  /**  char s1[98] = "Hello ";
   char s2[] = "World!\n";
   char *ptr;
 
@@ -20,7 +20,7 @@ int main (void)
   printf("%s", s1);
   printf("%s", s2);
   printf("%s", ptr);
-  return 0;
+  return 0; */
 
   /**  char s1[98];
   char *ptr;
@@ -53,13 +53,15 @@ int main (void)
   printf("\n");
   return (0); 
   */
-  /**  char s1[] = "Hello";
-  char s2[] = "World!";
+  
+  char s1[] = "World!";
+  char s2[] = "Hello";
 
   printf("%d\n", _strcmp(s1, s2));
   printf("%d\n", _strcmp(s2, s1));
   printf("%d\n", _strcmp(s1, s1));
-  return (0); */
+  return (0);
+  
   /**  int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
   print_array(a, sizeof(a) / sizeof(int));

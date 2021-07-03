8867c7e6e36edd7ac5242d01d560719a2fef6761
Jtobyy <jtobi8161@gmail.com>
2021-07-03 14:57:31 +0000

    Implement printf for ints with only _putchar - incomplete

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,12 +5,20 @@
  *@n: number to be printed
  *Return: void
  */
+int _pow10(int);
+
 void print_number(int n)
 {
 int quo;
+int rem;
 int i;
+int j;
+int p;
+int m;
+int n;
 i = 0;
 quo = n;
+rem = n % 10;
 if (n < 0)
 {
 putchar('-');
@@ -19,16 +27,52 @@ print_number(n);
 }
 while (quo >= 10)
 {
-    quo = n / 10;
-    printf("quo at this while: %d\", quo);
+    quo = quo / 10;
     i++;
-} 
+}
+ 
 putchar(quo + '0');
-n = (n - (quo*10*i));
+p = _pow10(i);
+n = (n - (quo * p));
+
+
+ if ((P / 10) > n)
+  {
+    m = p - n;
+{
+for (j = 0; j < i; j++)
+putchar('0');
+return;
+}
+else
+{
+for (j = 1; j < i; j++)
+putchar('0');
+putchar(n + '0');
+return;
+}
+
+  }
 if (n >= 10)
 print_number(n);
 else
-putchar(n + '0');
-printf("n at this last else: %d", n);
-return;
+{
+
+{
+}
+}
+
+  /**
+   *_pow - calculates the value of 10 raised to an integer
+   */
+int _pow10(int n)
+{
+int i;
+int tmp;
+tmp = 10;
+for (i = 1; i < n; i++)
+{
+tmp *= 10;
+}
+return (tmp);
 }

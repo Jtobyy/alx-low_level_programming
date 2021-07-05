57e2cd4b8bfbfc7ae8d9182f28f752777ea2c1a7
Jtobyy <jtobi8161@gmail.com>
2021-07-05 17:41:23 +0000

    Implement c's strchr fuction

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -6,6 +6,7 @@
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 int _putchar(char c);
 
 #endif
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,21 +1,31 @@
 #include "holberton.h"
 
-void simple_print_buffer(char *buffer, unsigned int size);
+/*void simple_print_buffer(char *buffer, unsigned int size);*/
 
 int main(void)
 {
-  char buffer[98] = {0};
+  /** char buffer[98] = {0};
   char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
   simple_print_buffer(buffer, 98);
   _memcpy(buffer + 50, buffer2, 10);
   printf("-------------------------------------------------\n");
   simple_print_buffer(buffer, 98);
+  return (0); */
+  char *s = "hello";
+  char *f;
+
+  f = _strchr(s, 'l');
+
+  if (f != NULL)
+    {
+      printf("%s\n", f);
+    }
   return (0);
 }
 
 
-void simple_print_buffer(char *buffer, unsigned int size)
+/** void simple_print_buffer(char *buffer, unsigned int size)
 {
   unsigned int i;
 
@@ -35,3 +45,4 @@ void simple_print_buffer(char *buffer, unsigned int size)
     }
   printf("\n");
 }
+*/

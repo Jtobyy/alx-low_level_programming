eeb6b8f8260bc15ebce3139fff64f5a0e8c45bc1
Jtobyy <jtobi8161@gmail.com>
2021-07-12 22:03:26 +0000

    Was mixing array of pointers with pointers to pointers - Finally figured that out

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,11 +6,14 @@
  *@argv: array of pointers containing cmd line ags
  *Return: int 0
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char **argv)
 {
 int i;
+int j;
 int a;
+char *p;
 a = 0;
+j = 0;
 if (argc < 2)
 {
 printf("%d\n", 0);
@@ -20,13 +23,18 @@ for (i = 0; i < argc; i++)
 {
 if (i == 0)
 continue;
-if (*(argv[i]) >= 48 && *(argv[i]) <= 57)
-a += atoi(argv[i]);
-else
+p = argv[i];
+while (*(p + j) != '\0')
+{
+if (*(p + j) < 48 || *(p + j) > 57)
 {
 printf("Error\n");
 return (1);
 }
+j++;
+}
+a += atoi(p);
+j = 0;
 }
 printf("%d\n", a);
 return (0);

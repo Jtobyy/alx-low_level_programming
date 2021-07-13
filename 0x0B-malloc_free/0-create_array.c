bc9b09f13f641481cb59197a4571af2a2c01396d
Jtobyy <jtobi8161@gmail.com>
2021-07-13 20:31:15 +0000

    Start using dynamic memory allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *create_array -  creates an array of chars, and
+ * initializes it with a specific char.
+ *@size: size of array
+ *@c: char to initialize arr to
+ *Return: pointer to arry
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *p;
+p = malloc(sizeof(char) * size);
+for (i = 0; i < size; i++)
+{
+p[i] = c;
+}
+if (p == 0)
+return (NULL);
+if (p)
+return (p);
+else
+return (NULL);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+
+#endif
+

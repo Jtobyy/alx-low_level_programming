4628ea7d5ac43d09a8a1ded33c5b25a794eedb37
Jtobyy <jtobi8161@gmail.com>
2021-07-14 13:05:45 +0000

    Make sure all previouly got memory are freed on error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,16 @@ p = (int **)malloc(sizeof(int *) * height);
 if (p == NULL)
 return (NULL);
 for (i = 0; i < height; i++)
+{
 p[i] = malloc(sizeof(int) * width);
+if (p[i] == NULL)
+{
+free(p);
+for (j = 0; j < i; j++)
+free(p[j]);
+return (NULL);
+}
+}
 for (i = 0; i < height; i++)
 {
 for (j = 0; j < width; j++)

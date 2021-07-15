71a92984b8937b534ddad0f70d572e7888689e95
Jtobyy <jtobi8161@gmail.com>
2021-07-15 20:04:56 +0000

    Create an array of range

--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -29,7 +29,7 @@
   free(concat);
   return (0);
 }*/
-void simple_print_buffer(char *buffer, unsigned int size)
+/**void simple_print_buffer(char *buffer, unsigned int size)
 {
   unsigned int i;
 
@@ -59,6 +59,35 @@ int main(void)
   strcpy(a + 9, " School! :)\n");
   a[97] = '!';
   simple_print_buffer(a, 98);
+  free(a);
+  return (0);
+  }*/
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+  unsigned int i;
+
+  i = 0;
+  while (i < size)
+    {
+      if (i % 10)
+	{
+	  printf(" ");
+	}
+      if (!(i % 10) && i)
+	{
+	  printf("\n");
+	}
+      printf("0x%02x", buffer[i]);
+      i++;
+    }
+  printf("\n");
+}
+int main(void)
+{
+  int *a;
+
+  a = array_range(0, 10);
+  simple_print_buffer(a, 11);
   free(a);
   return (0);
 }

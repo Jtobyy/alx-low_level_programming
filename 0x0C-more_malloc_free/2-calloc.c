6488d1eb84e19a2bec90574dedd0be8e97cd2e6a
Jtobyy <jtobi8161@gmail.com>
2021-07-16 02:15:55 +0000

    Fix ..

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ unsigned int i;
 char *p;
 if (nmemb == 0 || size == 0)
 return (NULL);
-p = malloc(nmemb * sizeof(size));
+p = malloc((size * nmemb) + size);
 if (p == NULL)
 return (NULL);
 for (i = 0; i < (nmemb * size); i++)

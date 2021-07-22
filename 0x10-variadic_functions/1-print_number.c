accbf0a0648e721a2c7d0d99fc520a41d5a1e604
Jtobyy <jtobi8161@gmail.com>
2021-07-22 23:23:04 +0000

    To be is to be the value of a variable: Print all parameters of a variadic funciton with a separator after eacg

---
+++ 0x10-variadic_functions/1-print_number.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - prints numbers
+ *@separator: string to be printed between numbers
+ *@n: number of integers passed to the funciton
+ *Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+if (separator == NULL)
+return;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ap, int));
+if (i != n - 1)
+printf("%s", separator);
+}
+putchar('\n');
+return;
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,6 @@
 #include <stdarg.h>
 
 int sum_them_all(unsigned int, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */

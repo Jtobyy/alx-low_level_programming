5b87d4714c515d6d27542c38980a898249a15e1a
Jtobyy <jtobi8161@gmail.com>
2021-07-22 23:29:05 +0000

    Correct a check bug

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,13 +10,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list ap;
 unsigned int i;
-if (separator == NULL)
-return;
 va_start(ap, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(ap, int));
-if (i != n - 1)
+if (i != n - 1 && separator != NULL)
 printf("%s", separator);
 }
 putchar('\n');

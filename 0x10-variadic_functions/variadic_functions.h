271e5b6a784281443ef51b50329a2b18ef64abeb
Jtobyy <jtobi8161@gmail.com>
2021-07-23 01:02:57 +0000

    Print all args passed to variadic func

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,6 @@
 int sum_them_all(unsigned int, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */

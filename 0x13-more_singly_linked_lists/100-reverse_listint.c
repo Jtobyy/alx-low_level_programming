d0292d51d3747131b56370df8692a95977077de4
Jtobyy <jtobi8161@gmail.com>
2021-08-05 00:52:29 +0000

    Work on function that reverses a linked list: 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,5 +7,40 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+listint_t *tmp;
+listint_t *tmp2 __attribute__((unused));
+tmp = NULL;
+if (*head == NULL)
+return (NULL);
+if ((*head)->next == NULL)
 return (*head);
+if (((*head)->next)->next == NULL)
+{
+tmp = (*head)->next;
+(*head)->next = NULL;
+tmp->next = *head;
+*head = tmp;
+return (*head);
+}
+tmp = (*head)->next;
+(*head)->next = NULL;
+tmp2 = tmp->next;
+tmp->next = *head;
+*head = tmp2->next;
+if (*head != NULL)
+{
+while (*head != NULL)
+{
+tmp2->next = tmp;
+tmp = (*head)->next;
+(*head)->next = tmp2;
+(*head) = tmp;
+}
+return (*head);
+}
+else
+{
+*head = tmp->next;
+return (*head);
+}
 }
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,8 @@ void free_listint2(listint_t **head)
 {
 listint_t *lp;
 lp = *head;
+if (*head == NULL)
+return;
 while (*head != NULL)
 {
 *head = lp->next;

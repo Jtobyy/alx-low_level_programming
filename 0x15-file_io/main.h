e492a840d4b3f6d682f28c615fa7cc2a6f907d0e
Jtobyy <jtobi8161@gmail.com>
2021-08-09 23:38:03 +0000

    created another main.h file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+
+#endif

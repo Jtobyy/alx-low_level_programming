602cf04b3cfdc16a9f6a595ab973a6c1be6a5d31
Jtobyy <jtobi8161@gmail.com>
2021-08-10 02:57:48 +0000

    changes to 1-create-file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,8 +10,6 @@ int create_file(const char *filename, char *text_content)
 {
 int f;
 int i;
-int w;
-int c;
 i = 0;
 if (filename == NULL)
 return (-1);
@@ -27,12 +25,8 @@ text_content[0] = '\0';
 }
 while (text_content[i] != '\0')
 i++;
-w = write(f, text_content, (i + 1));
-if (w == -1)
-return (-1);
-c = close(f);
-if (c = -1)
-return (-1);
+write(f, text_content, (i + 1));
+close(f);
 if (i == 0)
 free(text_content);
 return (1);

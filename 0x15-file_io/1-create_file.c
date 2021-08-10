26be7f8b0a4dd930b7b66a8c1c3689935e7a7ecf
Jtobyy <jtobi8161@gmail.com>
2021-08-10 01:34:50 +0000

    Write function that appends a text to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,7 @@ int create_file(const char *filename, char *text_content)
 int f;
 int i;
 i = 0;
-if (filename[0] == 'N' && filename[1] == 'U'
-&& filename[2] == 'L' && filename[3] == 'L')
+if (filename == NULL)
 return (-1);
 f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (f == -1)
@@ -26,7 +25,9 @@ text_content[0] = '\0';
 }
 while (text_content[i] != '\0')
 i++;
-write(f, text_content, i + 1);
+write(f, text_content, (i + 1));
 close(f);
+if (i == 1)
+free(text_content);
 return (1);
 }
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *append_text_to_file - appends a text to a file
+ *@filename: name of file
+ *@text_content: content to append
+ *Return: 1 on success and -1 otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int i;
+int f;
+i = 0;
+if (filename == NULL)
+return (-1);
+if (text_content == NULL)
+{
+text_content = malloc(sizeof(*text_content));
+text_content[0] = '\0';
+}
+f = open(filename, O_WRONLY | O_APPEND);
+if (f == -1)
+return (-1);
+while (text_content[i] != '\0')
+i++;
+if (i != 0)
+write(f, text_content, (i + 1));
+return (1);
+}

19f927b8eef2298aef5f2fc2b665757e5d021b19
Jtobyy <jtobi8161@gmail.com>
2021-10-08 19:57:40 -0400

    ..

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,8 +3,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *hash_table;
-hash_table = malloc(sizeof(hash_node_t) * size);
-if (hash_table == NULL)
+hash_node_t *array;
+hash_table = malloc(sizeof(hash_table_t));
+hash_table->size = size;
+array = malloc(sizeof(hash_node_t) * size);
+if (array == NULL)
 return NULL;
+hash_table->array = &array;
 return hash_table;
 }

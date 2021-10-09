496700a1ef0054fea19fb82722298d244e5f4e9a
Jtobyy <jtobi8161@gmail.com>
2021-10-08 20:02:00 -0400

    betty linter

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,18 @@
 #include "hash_tables.h"
 
+/**
+ *hash_table_create - creates a hash table
+ *
+ *@size: size of the hash table
+ *Return: poiter to table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *hash_table;
 hash_node_t *array;
 hash_table = malloc(sizeof(hash_table_t));
+if (hash_table == NULL)
+return NULL;
 hash_table->size = size;
 array = malloc(sizeof(hash_node_t) * size);
 if (array == NULL)

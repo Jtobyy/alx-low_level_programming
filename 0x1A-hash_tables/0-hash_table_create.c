990da883e1452929847e0c5cdfbc7b7b12e1a06b
Jtobyy <jtobi8161@gmail.com>
2021-10-13 23:15:43 +0000

    Add 1-djb2.c in 0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,11 @@ hash_table_t *hash_table;
 hash_node_t *array;
 hash_table = malloc(sizeof(hash_table_t));
 if (hash_table == NULL)
-return NULL;
+return (NULL);
 hash_table->size = size;
 array = malloc(sizeof(hash_node_t) * size);
 if (array == NULL)
-return NULL;
+return (NULL);
 hash_table->array = &array;
-return hash_table;
+return (hash_table);
 }
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,4 @@
+#include "hash_tables.h"
+
+
+unsigned long int hash_djb2(const unsigned char *str);

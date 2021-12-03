8c6d393fd3a0c210d1d1eb1e8dbc04961f4d0055
Jtobyy <jtobi8161@gmail.com>
2021-12-03 01:31:28 +0000

    Implement 5-Island-perimeter

---
+++ 0x1C-makefiles/5-island_perimeterold.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Implementation of function island_perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    """
+    island = []
+    width = len(grid[0])
+    height = 0;
+    for i in grid:
+        height += 1
+        edge = []
+        for j in i:
+            if j == 1:
+                edge.append(1)
+        if len(edge) != 0:
+            island.append(edge)
+    for ls in island:
+        i = 0
+        while i < height:
+            try:
+                if ls[i]:
+                    print(ls[i], end="")
+            except IndexError:
+                break;
+            i += 1;
+        print()
+
+        
+

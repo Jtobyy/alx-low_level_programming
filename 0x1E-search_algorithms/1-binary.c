8d50751b940a22763ea589b331384512324e453d
Jtobyy <jtobi8161@gmail.com>
2022-06-06 03:11:05 +0100

    Searches for a value in a sorted array of integers using the Binary search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+
+/**
+ * binary_search - searches for a value in an
+ * array of integers using the Binary search algorithm
+ * @array: Pointer to the first element of the array to
+ * serach in
+ * @size: Size of elements in the array
+ * @value: Value to search for
+ * array is sorted in ascending order
+ * value won't appear more than once
+ * Return: The index where the value is located
+ * or -1 if array is NULL or value is not present
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int j;
+	int cur;
+	int L;
+	int R;
+
+	L = 0;
+	R = size - 1;
+	if (L > R)
+		return (-1);
+	while (L <= R)
+	{
+		printf("Searching in array: ");
+		for (j = L; j < R + 1; j++)
+		{
+			if (j == R)
+				printf("%d\n", j);
+			else
+				printf("%d, ", j);
+		}
+		cur = (L + R) / 2;
+		if (array[cur] == value)
+			return (cur);
+		else if (array[cur] < value)
+			L = cur + 1;
+		else if (array[cur] > value)
+			R = cur - 1;
+	}
+	return (-1);
+}
